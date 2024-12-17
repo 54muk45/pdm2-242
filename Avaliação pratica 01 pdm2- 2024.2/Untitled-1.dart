b9196c714a9cfa804d34df559940e19790283b34
54muk45 <128948544+54muk45@users.noreply.github.com>
2024-12-17 17:34:27 -0300

    Add files via upload

---
+++ Avaliação pratica 01 pdm2- 2024.2/Untitled-1.dart
@@ -0,0 +1,47 @@
+import 'package:sqlite3/sqlite3.dart';
+
+void main() {
+  final db = sqlite3.openInMemory();
+
+  db.execute('''
+    CREATE TABLE IF NOT EXISTS TB_ALUNOS (
+      id INTEGER NOT NULL PRIMARY KEY,
+      name TEXT NOT NULL,
+      idade INTEGER NOT NULL
+    );
+  ''');
+  print('Tabela TB_ALUNOS criada com sucesso.');
+
+ 
+  db.execute('ALTER TABLE TB_ALUNOS RENAME TO TB_CONTRATADOS;');
+  
+
+  // 3. Adicionei aqu umas colunas com os  seguintes nomes'originario', 'certificação' e 'linguagem'
+  db.execute('ALTER TABLE TB_CONTRATADOS ADD COLUMN originario TEXT;');
+  db.execute('ALTER TABLE TB_CONTRATADOS ADD COLUMN certificação TEXT;');
+  db.execute('ALTER TABLE TB_CONTRATADOS ADD COLUMN linguagem TEXT;');
+  print('Colunas originario, certificação e linguagem adicionadas com sucesso.');
+
+  // 4. Inserir dados
+  final stmt = db.prepare('INSERT INTO TB_CONTRATADOS (name, idade, originario, certificação, linguagem) VALUES (?, ?, ?, ?, ?)');
+  stmt
+    ..execute(["Andre", 21, "fortaleza", "Curso A", "Dart"])
+    ..execute(["Vinijr", 25, "acarau", "Curso B", "Python"])
+    ..execute(["Almeida", 19, "fortaleza", "Curso C", "Java"])
+    ..execute(["Pedroca", 20, "caucaia", "Curso D", "C++"]);
+
+  stmt.dispose();
+  print('Dados inseridos na tabela TB_CONTRATADOS.');
+
+  // 5. Buscar dados na tabela
+  final resultSet = db.select('SELECT * FROM TB_CONTRATADOS');
+
+  print('Dados na tabela TB_CONTRATADOS:');
+  for (final row in resultSet) {
+    print(
+        'id: ${row['id']}, name: ${row['name']}, idade: ${row['idade']}, originario: ${row['originario']}, certificação: ${row['certificação']}, linguagem: ${row['linguagem']}');
+  }
+
+  
+  db.dispose();
+}
